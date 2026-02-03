6fcc5281d7eb61ffb96ac306398c19503919aedf
K1eran1 <254428749+K1eran1@users.noreply.github.com>
2026-02-03 12:04:23 +0000

    Build working drawing mechanics

--- script.js
+++ script.js
@@ -1,38 +1,76 @@
 const sketchBox = document.getElementById("sketchBox");
 const gridSizeInput = document.getElementById("gridSizeInput");
 const resetBtn = document.getElementById("resetBtn");
+const colourInput = document.getElementById("color");
+
+let isDrawing = false;
+
+document.addEventListener("mousedown", () => {
+    isDrawing = true;
+});
+
+document.addEventListener("mouseup", () => {
+    isDrawing = false;
+});
 
 function fillSketchBox(gridSize) {
 
     sketchBox.innerHTML = '';
 
-    let squareSize = 500 / gridSize
+    let squareSize = 500 / gridSize;
 
     const gridSizeSquared = gridSize * gridSize;
 
-    for (i = 0; i < gridSizeSquared; i++) {
+    for (let i = 0; i < gridSizeSquared; i++) {
         const gridSquare = document.createElement("div");
         gridSquare.classList = "square";
 
         sketchBox.appendChild(gridSquare);
+        gridSquare.style.width = `${squareSize}px`;
+        gridSquare.style.height = `${squareSize}px`;
+        gridSquare.style.boxSizing = "border-box";
 
-        gridSquare.style.width = `${squareSize}px`
-        gridSquare.style.width = `${squareSize}px`
-        gridSquare.style.boxSizing = "border-box"
-        gridSquare.style.border = "black solid 1px"
+        /*gridSquare.addEventListener("mouseenter", () => {
+            gridSquare.style.backgroundColor = "rgb(237, 237, 237)";
+        })
+
+        gridSquare.addEventListener("mouseleave", () => {
+            gridSquare.style.backgroundColor = "white";
+        })*/
+
+        function colorSquare() {
+            let currColor = colourInput.value;
+            gridSquare.style.backgroundColor = currColor;
+        }
+        
+        gridSquare.addEventListener("mouseover", () => {
+            if (isDrawing === true) {
+                colorSquare();
+                preventDefault();
+            }
+        });
+
+        gridSquare.addEventListener("click", () => {
+
+            colorSquare();
+            preventDefault();
+        });
     }
-}
+};
 
 resetBtn.addEventListener("click", () => {
-    let gridSize = parseInt(gridSizeInput.value);
+    gridSize = parseInt(gridSizeInput.value) || 16;
 
     if (gridSize <= 100 && 4 <= gridSize) {
         fillSketchBox(gridSize);
     }   else {
         return console.log("Please enter a value between 4 and 100");
     }
-})
+});
+
+fillSketchBox(16);
 
 
 
-fillSketchBox(16)
+
+
